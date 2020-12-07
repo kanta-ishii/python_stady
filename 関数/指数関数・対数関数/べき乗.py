2801cc53ca2769d49644aa90bef769eac2a6a398
kanta-ishii <kn.is.100000000@gmail.com>
2020-12-07 23:54:40 +0900

    No.10 指数関数・対数関数

---
+++ 関数/指数関数・対数関数/べき乗.py
@@ -0,0 +1,17 @@
+print(2**4)
+
+'''
+pow(x,y)
+    x: 定数 or 変数
+    y: 乗数
+'''
+import math
+
+print(pow(2,4))
+print(math.pow(2,4))
+
+
+'''
+pow(x,y,z) == pow(x, y) % z
+'''
+print(pow(2, 4, 5))
---
+++ 関数/指数関数・対数関数/平方根.py
@@ -0,0 +1,17 @@
+'''
+平方根（ルート）
+    **0.5 or math.sqrt()
+'''
+import math
+
+print(2**0.5)
+print(math.sqrt(2))
+print(2**0.5 == math.sqrt(2))
+
+'''
+複素数
+'''
+import cmath
+
+print(cmath.sqrt(-3 + 4j))
+print(cmath.sqrt(-1))
